f8a098815aada6fc0da573d8ba673559980b7e87
igril <igrilkul@hotmail.com>
2019-04-12 01:17:41 +0300

    Edited to work with reflection only

---
+++ C# OOP Advanced/7. Exam/TheTankGame/TheTankGame/Entities/Parts/ShellPart.cs
@@ -0,0 +1,21 @@
+﻿namespace TheTankGame.Entities.Parts
+{
+    using Contracts;
+
+    public class ShellPart :BasePart,IDefenseModifyingPart
+    {
+        public ShellPart(string model, double weight, decimal price, int defenceModifier)
+             : base(model, weight, price)
+        {
+            this.DefenseModifier = defenceModifier;
+        }
+
+
+        public int DefenseModifier { get; private set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"+{this.DefenseModifier} DefencePoints";
+        }
+    }
+}
